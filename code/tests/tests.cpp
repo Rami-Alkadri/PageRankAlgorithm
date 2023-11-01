62dbc6c25cd26569416771bc1af9d6c8e29b2771
Rami-Alkadri <113568805+Rami-Alkadri@users.noreply.github.com>
2023-10-31 20:39:13 -0500

    Fixed test cases

--- code/tests/tests.cpp
+++ code/tests/tests.cpp
@@ -1,13 +1,13 @@
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("basicSimHash1") {
+TEST_CASE("Different Sim Hash") {
     SimHash sh;
-    std::string doc1 = "This is a simple document.";
-    std::string doc2 = "This is a simple document!";
-    REQUIRE(sh.compute(doc1) != sh.compute(doc2));
+    std::string doc1 = "This is a complex document.";
+    std::string doc2 = "This is a simple document.";
+    REQUIRE(sh.computeSimHash(doc1) != sh.computeSimHash(doc2));
 }
 
-TEST_CASE("basicSimHash2")
+TEST_CASE("Same Sim Hash")
 {
     SimHash sh;
     std::string doc1 = "Hello, world!";
@@ -15,23 +15,31 @@ TEST_CASE("basicSimHash2")
     REQUIRE(sh.compute(doc1) == sh.compute(doc2));
 }
 
-TEST_CASE("similarityCheck1")
+TEST_CASE("High similarity")
 {
     SimHash sh;
     std::string doc1 = "The quick brown fox jumps over the lazy dog.";
     std::string doc2 = "The quick brown fox jumped over the lazy dogs.";
-    REQUIRE_FALSE(sh.isSimilar(doc1, doc2));
+    REQUIRE_FALSE(sh.measureSimilarity(doc1, doc2) <= .9);
 }
 
-TEST_CASE("similarityCheck2")
+TEST_CASE("Medium similarity")
 {
     SimHash sh;
     std::string doc1 = "Machine learning is a subset of artificial intelligence.";
     std::string doc2 = "Machine learning belongs to the domain of artificial intelligence.";
-    REQUIRE(sh.isSimilar(doc1, doc2));
+    REQUIRE(sh.measureSimilarity(doc1, doc2) >= .4 && sh.measureSimilarity(doc1, doc2) <= .6);
 }
 
-TEST_CASE("largeDocumentSimHash")
+TEST_CASE("Low similarity")
+{
+    SimHash sh;
+    std::string doc1 = "The quick brown fox jumps over the lazy dog.";
+    std::string doc2 = "Machine learning belongs to the domain of artificial intelligence.";
+    REQUIRE(sh.measureSimilarity(doc1, doc2) <= .1);
+}
+
+TEST_CASE("Large Doc SimHash")
 {
     SimHash sh;
     std::string doc1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.";
@@ -39,7 +47,7 @@ TEST_CASE("largeDocumentSimHash")
     REQUIRE(sh.compute(doc1) == sh.compute(doc2));
 }
 
-TEST_CASE("emptyDocumentSimHash")
+TEST_CASE("Empty Doc SimHash")
 {
     SimHash sh;
     std::string doc1 = "";
